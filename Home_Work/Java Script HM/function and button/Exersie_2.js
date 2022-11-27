09078a89d92cbe5a7aef93c8eb86f71e748eaf41
Eitan-Bellin <eitanbellin3@gmail.com>
2022-11-27 17:21:03 +0200

    Exercise 2

--- Home_Work/Java Script HM/function and button/Exersie_2.js
+++ Home_Work/Java Script HM/function and button/Exersie_2.js
@@ -3,6 +3,8 @@ function AddToFirstDiv() {
   let name = document.createTextNode(prompt);
   name = prompt("Plese enter yout name");
   first_div.append(name);
+  
+  //changing the button name
   new_name = document.getElementById("button");
   new_name.innerHTML = "Press me";
 }
