21f048e3e81ed2ee6e7a14bfa1ad05110d19aa6e
guy4213 <113546078+guy4213@users.noreply.github.com>
2022-12-04 14:34:36 +0200

    סיכום js&html

---
+++ 25 משימות js סיכום/12 onmouseover event! resize picture/12.js
@@ -0,0 +1,10 @@
+function bigImg(x) {
+  if (x.height<300&&x.width<300){
+  x.height += 100;
+  x.width += 100;
+}
+else alert("pic too big");
+
+
+}
+
---
+++ 25 משימות js סיכום/7/7.js
@@ -0,0 +1,19 @@
+
+
+function arrcall7() {
+
+let i,arr=[],x;
+       
+  
+      for(i=0;i<6;i++){
+      arr[i]= +prompt("enter num" +(i+1)+":");
+        }
+      x=Math.floor(Math.random() * i);
+        document.write(arr[x] +"<br/> num of arr is:  "+ (x+1));
+            
+      }
+   
+
+     // Update the 'innerHTML' property
+     //p_element_object.innerHTML = "Tomer Sagi";
+
