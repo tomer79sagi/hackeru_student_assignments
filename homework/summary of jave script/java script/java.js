c92d891a7677e984f215876a1748b936954b172a
Shimon4589 <shimon4589@gmail.com>
2022-12-10 18:42:47 +0200

    minure ficses

--- homework/summary of jave script/java script/java.js
+++ homework/summary of jave script/java script/java.js
@@ -1,10 +1,14 @@
-//----1-----//
-const SumOfNum = [10, 20, 30, 40, 50, 60, 73];
 
-function task1(SumOfNum) {
+const Numbers = [10, 20, 30, 40, 50, 60, 73];
+const vegetables = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
+    "cabbage"];
+
+//----1-----//
+
+function task1(Numbers) {
     let sum = 0;
-    for (let i = 0; i < SumOfNum.length; i++) {
-        sum += SumOfNum[i];
+    for (let i = 0; i < Numbers.length; i++) {
+        sum += Numbers[i];
     }
     document.getElementById("ptask1").innerHTML +=
         "<br/><br/> Sum of given array is " + sum;
@@ -12,31 +16,40 @@ function task1(SumOfNum) {
 
 //----2-----//
 
-const AveOfNum = [10, 20, 30, 40, 50, 60, 73];
-
-function task2(AveOfNum) {
+function task2(Numbers) {
     let sum = 0;
-    for (let i = 0; i < AveOfNum.length; i++) {
-        sum += AveOfNum[i];
+    for (let i = 0; i < Numbers.length; i++) {
+        sum += Numbers[i];
     }
     document.getElementById("ptask2").innerHTML +=
-        "<br/><br/> average of given array is " + sum / AveOfNum.length;
+        "<br/><br/> average of given array is " + sum / Numbers.length;
 }
 
 //----3-----//
 
-const CupleOfArry1 = [10, 20, 30, 40, 50, 60, 73];
-const CupleOfArry2 = [10, 20, 30, 40, 50, 60, 73];
+const CupleOfArry1 = Numbers
+const CupleOfArry2 = Numbers
 
 function task3(CupleOfArry1, CupleOfArry2) {
 
-    document.getElementById("ptask3.2").innerHTML += "<br/><br/>" + CupleOfArry1 + ", in your face " + CupleOfArry2;
+    document.getElementById("ptask3.2").innerHTML += "<br/><br/>" + CupleOfArry1 + ", in your face, " + CupleOfArry2;
 }
 
-//----4-----//
+// function task31(CupleOfArry1, CupleOfArry2) {
 
-const vegetables = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
+//     let array = CupleOfArry1;
+
+//     for (let i = 0; i < CupleOfArry2.length; i++) {
+//         let a = CupleOfArry2[i]
+//         array.push(a);
+//     }
+//     document.getElementById("ptask31").innerHTML +=
+//         "<br/><br/>" + array;
+// }
+    
+
+
+//----4-----//
 
 function task4(vegetables) {
     let longest = 0;
@@ -54,39 +67,36 @@ function task4(vegetables) {
 
 //----5-----//
 
-const vegetables1 = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
-
-function task5(vegetables1) {
+function task5(vegetables) {
     let longest = 0;
     let length = 0;
 
-    for (let i = 0; i < vegetables1.length; i++) {
-        if (vegetables1[i].length > length) {
-            length = vegetables1[i].length;
-            longest = vegetables1[i];
+    for (let i = 0; i < vegetables.length; i++) {
+        if (vegetables[i].length > length) {
+            length = vegetables[i].length;
+            longest = vegetables[i];
         }
     }
     document.getElementById("ptask5").innerHTML +=
-        "<br/><br/>" + length;
+        "<br/><br/> the longest number is " + length + " letters";
 
     let short = length;
 
-    for (let i = 0; i < vegetables1.length; i++) {
-        if (vegetables1[i].length < short) {
-            short = vegetables1[i].length;
-            longest = vegetables1[i];
+    for (let i = 0; i < vegetables.length; i++) {
+        if (vegetables[i].length < short) {
+            short = vegetables[i].length;
+            longest = vegetables[i];
         }
     }
     document.getElementById("ptask5").innerHTML +=
-        "<br/><br/>" + short;
+        "<br/><br/> the shortest number is " + short + " letters";
 
     if (short === length) {
         document.getElementById("ptask5").innerHTML +=
-            "<br/><br/>" + true;
+            "<br/><br/> therefore, the answer to the question is " + true;
     } else {
         document.getElementById("ptask5").innerHTML +=
-            "<br/><br/>" + false;
+            "<br/><br/> therefore, the answer to the question is " + false;
     }
 }
 
@@ -110,81 +120,65 @@ function task6(x, y) {
 
 //----7-----//
 
-const vegetables2 = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
-
-function task7(vegetables2) {
-    let newVagetable = vegetables2[Math.floor(Math.random() * vegetables2.length)];
+function task7(vegetables) {
+    let newVagetable = vegetables[Math.floor(Math.random() * vegetables.length)];
 
     document.getElementById("ptask7").innerHTML += "<br/><br/>" + newVagetable;
 }
 
 //----8-----//
 
-const vegetables3 = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
-
-function task8(vegetables3) {
-    let first = vegetables3[0];
-    let last = vegetables3[vegetables3.length - 1];
+function task8(vegetables) {
+    let first = vegetables[0];
+    let last = vegetables[vegetables.length - 1];
 
     document.getElementById("ptask8").innerHTML += "<br/><br/>" + "the first element is = " + first + "<br/>" + "and the lest element is = " + last;
 }
 
 //----9-----//
 
-const vegetables4 = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
+let FirstLetter = [];
 
-
-    let FirstLetter = [];
-
-    function task9(vegetables4) {
-      for (let i = 0; i < vegetables4.length; i++) {
-        FirstLetter += vegetables4[i].charAt(0).toUpperCase() + vegetables4[i].slice(1) + " ";
-      }
-      document.getElementById("ptask9").innerHTML += "<br/><br/>" + FirstLetter;
+function task9(vegetables) {
+    for (let i = 0; i < vegetables.length; i++) {
+        FirstLetter += vegetables[i].charAt(0).toUpperCase() + vegetables[i].slice(1) + " ";
     }
-    
-    //----10-----//
+    document.getElementById("ptask9").innerHTML += "<br/><br/>" + FirstLetter;
+}
 
-    const vegetables5 = ["cucumber", "tomato", "potato", "onion", "eggplant", "beet",
-    "cabbage"];
+//----10-----//
 
-    
-function task10(vegetables5) {
+function task10(vegetables) {
     let newarray = [];
-    for (let i = 0; i < vegetables5.length; i++) {
-      if (vegetables5[i].length >= 6) {
-        newarray.push(vegetables5[i]);
-      }
+    for (let i = 0; i < vegetables.length; i++) {
+        if (vegetables[i].length >= 6) {
+            newarray.push(vegetables[i]);
+        }
     }
     document.getElementById("ptask10").innerHTML += "<br/><br/>" + newarray;
-  }
-  
-  //----11-----//
+}
 
-  function task11() {
+//----11-----//
+
+function task11() {
     let btn = document.getElementById("task11");
-  
-    if (btn.innerHTML === "fuck") {
-      btn.innerHTML = "you";
-      btn.style.backgroundColor = "red";
-    } else if (btn.innerHTML === "you") {
-      btn.innerHTML = "fuck";
-      btn.style.backgroundColor = "yellow";
-    }
-  }
-  
 
-  //----12-----//
-  function big(x) {
+    if (btn.innerHTML === "fuck") {
+        btn.innerHTML = "you";
+        btn.style.backgroundColor = "red";
+    } else if (btn.innerHTML === "you") {
+        btn.innerHTML = "fuck";
+        btn.style.backgroundColor = "yellow";
+    }
+}
+
+//----12-----//
+function big(x) {
     x.style.height = "500px";
     x.style.width = "500px";
-  }
-  
-  function normal(x) {
+}
+
+function normal(x) {
     x.style.height = "5px";
     x.style.width = "5px";
-  }
-  
+}
