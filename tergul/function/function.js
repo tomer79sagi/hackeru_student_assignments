6ba618b64f1105b8018e0c93d880032b4f453d61
Shimon4589 <shimon4589@gmail.com>
2022-12-17 17:21:12 +0200

    3

--- tergul/function/function.js
+++ tergul/function/function.js
@@ -1,10 +1,42 @@
 
-function one(num1, num2) {
-    num1 = document.getElementById("1_1")
-    num2 = document.getElementById("1_2")
+function one() {
+    let num1 = document.getElementById("1_1").value
+    let num2 = document.getElementById("1_2").value
 
-    if (num1 > num2) {
-        document.getElementById("one").innerHTML+=num1;
-    } else
-        document.getElementById("one").innerHTML+=num2;
+    let big_number = Math.max(num1,num2)
+    document.getElementById("one").innerHTML = big_number;
 }
+
+function two() {
+    let num1 = document.getElementById("2_1").value
+    let num2 = document.getElementById("2_2").value
+
+    let power_number = Math.pow(num1,num2)
+    document.getElementById("two").innerHTML = power_number;
+}
+
+function three(){
+    let number = document.getElementById("3").value
+    let num = 1
+
+    while(number > 0){
+        num = num * number;
+        number--;
+    }
+    document.getElementById("three").innerHTML = num;
+}
+
+
+// var number = +prompt("enter number");
+// var num = 1;
+
+
+// else {
+//     while (number > 0) {
+//         document.writeln(number);
+//         num = num * number;
+//         number--;
+
+//     }
+// }
+// document.write(num);
