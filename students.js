4b6ee3794dc38b678a24641076d8f1e60b1d7445
Alexey Golov <alexgolov@gmail.com>
2022-12-24 15:44:13 +0200

    students

---
+++ students.js
@@ -0,0 +1,19 @@
+const Sudentstatus = {
+  ACCEPTED: 1,
+  REJECTEC: 2,
+  COMPLETED: 3,
+  WINNER: 4,
+};
+class students {
+  Status = Studentstatus.Accepted;
+}
+let stude_1 = new students();
+let stude_2 = new students();
+let stude_3 = new students();
+let stude_4 = new students();
+
+arr=[stude_1,stude_2,stude_3,stude_4];]
+for(let i=0;i<students.length;i++) {
+  console.log()
+}
+  
