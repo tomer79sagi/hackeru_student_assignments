85743e1f49ce06d2071c112aac069348e0de1bd6
yarin2003 <yarinb0307@gmail.com>
2023-01-02 15:23:33 +0200

    in process

--- final project/final_javascript_project.js
+++ final project/final_javascript_project.js
@@ -31,32 +31,31 @@ class TaskManager {
     //methods
     //add
     add(description, id){
-        let create_task = new Task(description,id);
+        let key = this.gen_key(id[0])
+        let create_task = new Task(description,key);
         //console.log(create_task);
-        this.Tasks_obj[id] = create_task;
+        this.Tasks_obj[key] = create_task;
+        localStorage.setItem(key,JSON.stringify(create_task))
     }
 
     //delete
     delete_task(id){
         let element = document.getElementById(id[0]);
         document.getElementById("divs_container").removeChild(element)
-        this.Tasks_obj.splice(id[0], 1);
+        this.Tasks_obj[id[0]] = null
         console.log(this.Tasks_obj);
+        
     }
 
     //edit
     edit_task(id){
-        currentJob = id[0]
-        console.log(id);
-        console.log(typeof(edit_btn));
-        console.log(typeof(edit_inp));
+        currentJob = id[0];
         edit_inp.id = "edit_input";
         edit_inp.value = document.getElementById(id[0]+"desc").innerHTML;   
         edit_btn.onclick = change_description;
         edit_btn.id = "edit_button";
         document.body.appendChild(edit_inp);
         document.body.appendChild(edit_btn);
-        
     }
 
     //change status
@@ -75,16 +74,29 @@ class TaskManager {
         let doc = document.getElementById(id+"status");
         doc.innerHTML = taskManager.Tasks_obj[id].status;
     }
-}
 
-
-function change_description(id, edit_inp){
-        let input = document.getElementById("edit_input")
-        taskManager.Tasks_obj[currentJob].description = input.value;
-        document.getElementById(currentJob).innerHTML = input.value;
+    //preventing empty cells in array
+    gen_key(key){
+        for(let i = 0;i <= this.Tasks_obj.length; ++i){
+            if(this.Tasks_obj[i]){
+                continue
+            }
+            else{
+                return i
+            }
+        }
+        return key++
+    }
 }
 
 let currentJob = 0
+
+function change_description(){
+        let input = document.getElementById("edit_input")
+        taskManager.Tasks_obj[currentJob].description = input.value;
+        document.getElementById(currentJob+"desc").innerHTML = input.value;
+        delete_edit_input()
+}
 
 //enum for status
 const TaskStatus = {
@@ -130,7 +142,30 @@ function delete_input(){
     btn.remove();
 }
 
+function delete_edit_input(){
+    let edit_inp = document.getElementById("edit_input")
+    let edit_btn = document.getElementById("edit_button");
+    edit_inp.remove()
+    edit_btn.remove()
+}
+
 //elements for edit_task
 edit_inp = document.createElement("input");
 edit_btn = document.createElement("button");
 edit_btn.innerHTML = "Edit";
+
+
+
+function printDate(){
+    let tod_d = new Date();
+    console.log(tod_d);
+    let my_d = document.getElementById("d");
+    console.log(my_d.value);
+    //let day = tod_d.getDate().toString();
+    //let month = (tod_d.getMonth()+1).toString();
+    //let year = tod_d.getFullYear().toString();
+    //let today = year + "-0" + month + "-0" + day;
+    //console.log(today);
+    console.log(my_d == tod_d);
+
+}
