c799dddba9b19b6bbf3e650c707171fd4ac95b72
Eitan-Bellin <eitanbellin3@gmail.com>
2023-01-03 21:33:08 +0200

    js project

--- Home_Work/Java Script HM/JS_summery_project/taskManeger.js
+++ Home_Work/Java Script HM/JS_summery_project/taskManeger.js
@@ -1,49 +1,83 @@
-//משתנה שמכיל את כל הכפתורים הנדרשים
-const TaskArray = [];
-let taskNumber = TaskArray.length;
+/**
+ * @file taskManeger.js
+ *
+ * @copyright Copyright 2022 Eytan Bellin | All rights
+ *                         to reserved to Eytan Bellin.
+ *
+ * @project    Task Manager
+ * @date       December 27, 2022
+ * @brief      This file comprises the implementation
+ *                         of the Task Manager module.
+ *******************************************************************************/
 
-// a new array to put tasks inside
+/******************************************************************************/
+/*     ENUMS                                                                  */
+/******************************************************************************/
 
-//serial number of task in array
-
-//Enum
 const TaskStatus = {
   UNCOMPLETED: 1,
   COMPLETED: 2,
 };
 
+/******************************************************************************/
+/*     CLASSES IMPLEMENTATION                                                 */
+/******************************************************************************/
+
+/* Task Class */
 class Task {
+  /* Class Attributes */
   status = TaskStatus.UNCOMPLETED;
   description = "";
-  //task id / serial number
   id = taskNumber;
 }
 
+/* Task Manager Class */
 class TaskManager {
-  //function that add tasks
-  add(task) {
-    //task description
-    task.description = prompt("enter your task");
-    let theTask = `
-        <div>
-          ${task.description}
+  /* Class Functions: */
+  /* 1. AddTask       */
+  /* 2. DeleteTask    */
+  /* 3. EditTask      */
+  /* 4. CompletTask   */
+
+  /**
+   * This function adds a new task.
+   * Executed when AddTask button is pressed.
+   *
+   * @param  task - A new instance of Task class
+   *
+   * @return None
+   */
+  AddTask(task) {
+    /* Task description */
+    task.description = prompt("Enter your task here :");
+
+    /* Create all task elements */
+    let taskStructure = `
+        <div id="desc_${task.id}">
+            ${task.description}
         </div>
+
         <div>
-          <button onclick="CompletTask(this)">Complete task</button>
-          <button onclick="taskManager.delete(this)">Delete task</button> <!-- שינוי הכפתור למתודה בתוך הכלאס -->
-          <button onclick="taskManager.change(this)">Change task</button> <!-- שינוי הכפתור למתודה בתוך הכלאס -->
-          <input type="checkbox" id="checkboxID_${task.id}">
-          <label for="checkboxID_${task.id}">completed</label>
+            <button onclick="taskManager.CompletTask(this)">Complete task</button>
+
+            <button onclick="taskManager.DeleteTask(this)">Delete task</button>
+          
+            <button onclick="taskManager.EditTask(this)">Change task</button>
+          
+            <input type="checkbox" id="checkboxID_${task.id}">
+
+            <label for="checkboxID_${task.id}>completed</label>
         </div>
         `;
+
     // add new div inside the "father div" that we gonna add to it the tasks buttonns and description
     let newDiv = document.createElement("div");
 
-    // add task id to the new div
+    /* Assign the task id to the new div */
     newDiv.id = task.id;
 
     //add the description and the buttons to the new div
-    newDiv.innerHTML = theTask + "<br><br>";
+    newDiv.innerHTML = taskStructure + "<br><br>";
 
     //set the div id as the "big one"
     let the_big_one = document.querySelector(".newTask");
@@ -52,58 +86,112 @@ class TaskManager {
     the_big_one.append(newDiv);
 
     //add "task" to task array
-    TaskArray.push(task);
-
+    taskArray.push(task);
     //print the array every task
-    console.log(TaskArray);
+    console.log(taskArray);
     // add counter +1 every task
     taskNumber++;
   }
 
-  // function for the delete button
-  delete(button) {
-    // x == newDiv id
+  /**
+   * This function deletes an existing task.
+   * Executed when DeleteTask button is pressed.
+   *
+   * @param  button - The button that triggered the function
+   *
+   * @return None
+   */
+  DeleteTask(button) {
     let x = button.parentNode.parentNode.id;
     console.log(x);
+
     button.parentNode.parentNode.remove();
 
-    TaskArray.forEach((task) => {
+    taskArray.forEach((task) => {
       if (task.id == x) {
         //עובר על המערך ומחפש איי די שדומה לאיקס ואז מוציא אותו מהמערך
-        TaskArray.splice(TaskArray.indexOf(task), 1);
+        taskArray.splice(taskArray.indexOf(task), 1);
       }
     });
 
     //show array value after deleting
-    console.log(TaskArray);
+    console.log(taskArray);
   }
 
-  //function that changing the description
-  change(button) {
+  /**
+   * This function allowas editing the description of an existing task.
+   * Executed when EditTask button is pressed.
+   *
+   * @param  button - The button that triggered the function
+   *
+   * @return None
+   */
+  EditTask(button) {
     let x = button.parentNode.parentNode.id;
-
     let taskName = prompt("Change your task");
+
     //new varieble that add a new discription and adds all buttons
     button.parentNode.parentNode.childNodes[1].textContent = taskName;
-    TaskArray.forEach((task) => {
+
+    taskArray.forEach((task) => {
       if (task.id == x) {
         task.description = taskName;
       }
     });
-    console.log(TaskArray);
+
+    console.log(x);
+    console.log(taskArray);
+  }
+
+  /**
+   * This function completes an existing task.
+   * Executed when CompletTask button is pressed.
+   *
+   * @param  button - The button that triggered the function
+   *
+   * @return None
+   */
+  CompletTask(button) {
+    let x = button.parentNode.parentNode.id;
+
+    taskArray.forEach((task) => {
+      if (task.id == x) {
+        task.status = TaskStatus.COMPLETED;
+        //container div
+        let taskDiv = document.getElementById(task.id);
+        //description div
+        let taskDesc = document.getElementById(`desc_${task.id}`)
+
+        // disable buttons
+        taskDiv.querySelectorAll('button')
+          .forEach(button => button.disabled = true)
+        // disable checkbox
+        taskDiv.querySelectorAll('input')
+          .forEach(button => button.disabled = true)
+        taskDesc.style.textDecoration = "line-through";
+        let checkbox = taskDiv.querySelector(`#checkboxID_${task.id}`);
+        checkbox.checked = true;
+      }
+    });
+    console.log(taskArray);
   }
 }
 
+/******************************************************************************/
+/*     GLOBAL VARIABLES                                                       */
+/******************************************************************************/
+
+/* Create Tasks array */
+const taskArray = [];
+
+/* Create Task Manager instance */
 let taskManager = new TaskManager();
+let taskNumber = taskArray.length;
 
-//הגדרת משתנה חדש לכפתור משימה חדשה
-let Add_new_task = document.getElementById("Add_task");
+/* Define Add Task Button */
+let add_task_button = document.getElementById("Add_task");
 
-//הגדרת כפתור לחיץ לאיידי
-Add_new_task.onclick = () => {
-  taskManager.add(new Task());
+/* Define OnClick action */
+add_task_button.onclick = () => {
+  taskManager.AddTask(new Task());
 };
-
-function CompletTask(button) {
-  button.parentNode;
-}
