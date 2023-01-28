a95a3d95b98c6fe06ca5c7706b17fa271211db81
AviadVa <aviadvaknin6766@gmail.com>
2023-01-28 20:08:01 +0200

    react project

---
+++ React/src/App.js
@@ -0,0 +1,19 @@
+import PageHeader from "./components/PageHeader";
+import PageMain from "./components/PageMain";
+
+import './App.css';
+
+
+function App() { 
+
+  return (
+    <div className="holy-grail">
+      <PageHeader/>
+      <PageMain/>
+   
+    </div>  
+
+  );
+}
+
+export default App;
---
+++ React/src/components/PageHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PageHeader.css";
+
+const pageHeader = () => {
+  return (
+    <header className="p-3 mb-2 bg-secondary text-white">
+      Customer realationship management
+    </header>
+  );
+};
+
+export default pageHeader;
