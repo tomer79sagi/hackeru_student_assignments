ad5b5b52cbd20c1487dd91e657ac713bdb1836ab
guy4213 <113546078+guy4213@users.noreply.github.com>
2023-01-28 20:34:36 +0200

    reactproject! enjoy

---
+++ reactproforgit/theproject/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    
+    return ( 
+        <footer >
+            <div className='foot'>
+                <span> contact us for updates :<span className='phone'  >0534271418</span></span>
+            </div>
+
+
+
+
+
+
+
+        </footer>
+     );
+}
+ 
+export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
